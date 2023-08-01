3571bc7c3b29c801999dbeeeb995d4830d1e5b86
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-01 11:01:49 -0400

    correción de grid y vinculación de data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,6 @@
+// El corazón de este proyecto es la manipulación de datos a través de arreglos y objetos.
+// Recomendamos que este archivo contenga toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones)
+
 // estas funciones son de ejemplo
 
 export const example = () => {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
+//Para todo el código que tenga que ver con mostrar los datos en la pantalla. Con esto nos referimos básicamente a la interacción con el DOM.
+// Operaciones como creación de nodos, registro de manejadores de eventos (event listeners o event handlers)...
+
+// INCLUIDO EN FORMATO //
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js'
+
+console.log(data.films);
+
+//const cant = data.films.length;
+//console.log(cant);
+
+
