d5e1b5387e90793aee9d6c241a38a7b269155224
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-01 17:08:59 -0400

    actualización para Eve

--- src/data.js
+++ src/data.js
@@ -3,10 +3,12 @@
 
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+    
--- src/main.js
+++ src/main.js
@@ -14,4 +14,37 @@ console.log(data.films);
 //const cant = data.films.length;
 //console.log(cant);
 
+function searchFilms() {
+    const searchInput = document.getElementById("searchInput").value;
+    const searchResults = document.getElementById("searchResults");
+
+    // Limpia los resultados anteriores
+    searchResults.innerHTML = "";
+
+    // Filtra las películas que coincidan con la búsqueda
+    const results = data.films.filter((film) =>
+    film.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    if (results.length === 0) {
+    searchResults.innerHTML = "No se encontraron resultados.";
+    return;
+    }
+
+    // Muestra los resultados
+    results.forEach((film) => {
+    const filmDiv = document.createElement("div");
+    filmDiv.innerHTML = `
+        <h3>${film.title}</h3>
+        <p>${film.description}</p>
+        <img src="${film.poster}" alt="${film.title}" width="200">
+        <p>Director: ${film.director}</p>
+        <p>Productor: ${film.producer}</p>
+        <p>Fecha de lanzamiento: ${film.release_date}</p>
+        <p>Puntuación Rotten Tomatoes: ${film.rt_score}</p>
+    `;
+    searchResults.appendChild(filmDiv);
+    });
+}  
+
 
