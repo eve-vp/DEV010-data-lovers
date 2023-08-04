f47fa82e021a37f163094db40ae1d428ec4bc43a
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-04 10:49:06 -0400

    cambios genesis y otros

--- src/main.js
+++ src/main.js
@@ -1,28 +1,19 @@
 import data from './data/ghibli/ghibli.js'
 
-
-
-// queremos que cuando el usuario escriba el nombre de la película y le traiga la imagen //
-const searchTitle = 'Castle in the Sky';
-// Utilizamos el método 'find()' para buscar la película por su título
-const foundFilm = data.films.find((film) => film.title === searchTitle);
-// Mostramos la película encontrada en la consola
-// console.log(foundFilm);
-
 function printdata(array){
   const container = document.querySelector('.movie-grid')
-  for(let i = 0 ; i< array.length; i++){
+  for(let i = 0 ; i < array.length; i++){
     // console.log(array[i]);
-    // Esto se vino del HTML -->
     container.innerHTML += ` <figure>  
         <img
           src="${array[i].poster}"
           alt="${array[i].title}"
         />
         <figcaption>${array[i].title}</figcaption>
-      </figure>` //template string
+      </figure>` //template string, estructura viene del HTML
   }
 }
+
 printdata(data.films);
 
 
