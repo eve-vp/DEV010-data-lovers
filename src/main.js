8c3ba48109c7290863dd72687123dd9cf568a1b6
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-04 14:23:48 -0400

    necesito cambios actualizados

--- src/main.js
+++ src/main.js
@@ -23,9 +23,57 @@ function printdata(array){
 }
 printdata(data.films)
 
+
+
+
+
 /// BUSCADOOOOOOOOORRRRRRR 
 const searchTitle = 'Castle in the Sky';
 // Utilizamos el método 'find()' para buscar la película por su título
 const foundFilm = data.films.find((film) => film.title === searchTitle);
 // Mostramos la película encontrada en la consola
 // console.log(foundFilm);
+
+let films = data.films;
+
+// METODO MAP
+// const titles = films.map((title) => title.title);
+// console.log(titles);
+
+// const directors = films.map((director) => director.director);
+// console.log(directors);
+
+// const releaseDates = films.map((releaseDate) => releaseDate.release_date);
+// console.log(releaseDates); 
+
+//METODO FLAT //Muestra todos los niveles, pero entrega TODA la información
+// const characters = films.flat(Infinity);
+// console.log(characters);
+
+
+
+
+
+//BUSCADOR EN TIEMPO REAL // NO FUNCIONA
+
+// const searcher = document.querySelector('input[placeholder="Search films"]');
+
+// const containerTitles = document.querySelector(".contenedor-card");
+// renderFilms(films, containerTitles, modalTitle);
+
+// searcher.addEventListener("keyup", (e) => {
+// //filtramos convirtiendo en minúsculas()
+// //usamos includes para comparar el valor 
+//   const results = films.filter(
+//     (film) =>
+//       film.title.toLocaleLowerCase().includes(e.target.value) ||
+//       film.director.toLocaleLowerCase().includes(e.target.value)
+//   );
+
+//   renderFilms(results, containerTitles, modalTitle);
+// });
+
+
+
+
+
