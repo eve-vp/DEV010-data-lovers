8484155facd91c719870182e28b5a77d62be2f67
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-08 13:40:53 -0400

    ordena funciones en main y data

--- src/data.js
+++ src/data.js
@@ -2,3 +2,11 @@
 //   return 'OMG';
 // };
 
+// Función pura para filtrar películas
+function filterMoviesByTitle(movies, searchTerm) {
+  return movies.filter((film) =>
+    film.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+}
+
+export { filterMoviesByTitle };
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,7 +1,7 @@
-import { example, anotherExample } from '../src/data.js';
+import { showMatchingFilms, anotherExample } from '../src/data.js';
 
 
-describe('example', () => {
+describe(`movie search container`, () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
   });
@@ -21,3 +21,15 @@ describe('anotherExample', () => {
     expect(anotherExample()).toBe('OMG');
   });
 });
+
+
+//1. unidad que esta siendo testeada
+describe('Products Service', function() {
+    describe('Add new product', function() {
+      //2. escenario y 3. quá se espera
+      it('When no price is specified, then the product status is pending approval', ()=> {
+        const newProduct = new ProductService().add(...);
+        expect(newProduct.status).to.equal('pendingApproval');
+      });
+    });
+  });
