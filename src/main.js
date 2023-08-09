16261e2aac3e798e35f464151818572854d2bd55
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-08 20:01:12 -0400

    Probando test

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import data from './data/ghibli/ghibli.js';
 import { filterMoviesByTitle } from './data.js';
 
 // Función para crear el HTML de una película
+// Recibe un objeto movie como argumento y devuelve una cadena de texto en formato HTML
+// Contiene una estructura de figura que incluye una imagen con la URL del póster y el título de la película como atributos
 function createMovieHTML(movie) {
   return `
     <figure>
@@ -14,10 +16,9 @@ function createMovieHTML(movie) {
 }
 
 // Función para mostrar las películas que coinciden con el término de búsqueda
-function showMatchingFilms(movies, searchTerm, container) {
-  const matchingFilms = filterMoviesByTitle(movies, searchTerm);
-  renderMovies(matchingFilms, container);
-}
+// Recibe tres parámetros (movies, searchTerm y container), luego llama a la función filterMoviesByTitle para filtrar las películas 
+const matchingFilms = filterMoviesByTitle(movies, searchTerm);
+renderMovies(matchingFilms, container);
 
 // Función para renderizar las películas en el contenedor
 // Toma un objeto de película y devuelve una cadena de HTML formateada con la información de la película. Esto separa la creación del contenido HTML de la función de renderizado principal.
@@ -28,6 +29,7 @@ function renderMovies(movies, container) {
 }
 
 // Obtén los elementos del campo de entrada y el botón de búsqueda
+// Crea tres constantes (searchImput, refreshButton y container)
 const searchInput = document.querySelector('#searchInput');
 const refreshButton = document.querySelector('#refreshButton');
 const container = document.querySelector('.movie-grid');
