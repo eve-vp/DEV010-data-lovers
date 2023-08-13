bba7a1d61cb1a12e93a8bb5f3260ce78a9929561
Rocio Lorca <lorcavillalobos@gmail.com>
2023-08-12 21:39:22 -0400

    OK search and sort by

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Importa los datos
 import data from './data/ghibli/ghibli.js';
-import { filterMoviesByTitle , sortByReleaseDate } from './data.js';
+import { filterMoviesByTitle , sortByReleaseDate, sortByTitle } from './data.js';
 
 // Función para crear el HTML de una película
 // Recibe un objeto movie como argumento y devuelve una cadena de texto en formato HTML
@@ -37,7 +37,7 @@ function showMatchingFilms(movies, searchTerm, container) {
 function renderMovies(movies, container) {
   const moviesHTML = movies.map(createMovieHTML).join('');
   container.innerHTML = moviesHTML;
-///////////////////////////////////////
+  ///////////////////////////////////////
 
   // Asegurarse de que todas las imágenes tengan el mismo tamaño
   const movieImages = container.querySelectorAll('img');
@@ -70,48 +70,42 @@ searchInput.addEventListener('keyup', () => {
   }
 });
 
-// Agrega un evento click al botón de actualización
-// refreshButton.addEventListener('click', () => {
-//   container.value = '';
-//   renderMovies(data.films, container);
-// });
-
 //*************************************/
 // Obtén el elemento del popup y los elementos para mostrar la información
-const popup = document.getElementById('popup');
-const popupTitle = document.getElementById('popupTitle');
-const popupDescription = document.getElementById('popupDescription');
-const closePopupButton = document.getElementById('closePopup');
+// const popup = document.getElementById('popup');
+// const popupTitle = document.getElementById('popupTitle');
+// const popupDescription = document.getElementById('popupDescription');
+// const closePopupButton = document.getElementById('closePopup');
 
 // Agrega un evento click a las imágenes de las películas
-container.addEventListener('click', (event) => {
-  const movieImage = event.target.closest('img');
-  if (movieImage) {
-    const movieId = movieImage.dataset.movieId; // Asegúrate de tener un atributo 'data-movie-id' en la imagen
-    const selectedMovie = data.films.find(movie => movie.id === movieId);
+// container.addEventListener('click', (event) => {
+//   const movieImage = event.target.closest('img');
+//   if (movieImage) {
+//     const movieId = movieImage.dataset.movieId; // Asegúrate de tener un atributo 'data-movie-id' en la imagen
+//     const selectedMovie = data.films.find(movie => movie.id === movieId);
     
-    if (selectedMovie) {
-      popupTitle.textContent = selectedMovie.title;
-      popupDescription.textContent = selectedMovie.description;
-      // Actualiza más detalles del popup con la información de la película
-      popup.style.display = 'block';
-    }
-  }
-});
+//     if (selectedMovie) {
+//       popupTitle.textContent = selectedMovie.title;
+//       popupDescription.textContent = selectedMovie.description;
+//       Actualiza más detalles del popup con la información de la película
+//       popup.style.display = 'block';
+//     }
+//   }
+// });
 
 // Agrega un evento click para cerrar el popup
-closePopupButton.addEventListener('click', () => {
-  popup.style.display = 'none';
-});
+// closePopupButton.addEventListener('click', () => {
+//   popup.style.display = 'none';
+// });
 
 // Agrega un evento click a las imágenes para abrir el popup
-container.addEventListener('click', event => {
-  const clickedImage = event.target;
-  const movieId = clickedImage.getAttribute('data-movie-id');
-  if (movieId) {
-    openPopup(movieId);
-  }
-});
+// container.addEventListener('click', event => {
+//   const clickedImage = event.target;
+//   const movieId = clickedImage.getAttribute('data-movie-id');
+//   if (movieId) {
+//     openPopup(movieId);
+//   }
+// });
 
 /////**********************************/
 
